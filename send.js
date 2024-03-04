1322068098ac87d06959a614d1875e3d72226f61
The PROgrammer / <codingplusgaming@gmail.com>
2024-03-03 17:08:24 -0800

    done

--- send.js
+++ send.js
@@ -1,5 +1,16 @@
+const d = new Date();
+
+window.onload = function() {
+  send();
+};
+
 var templateParams = {
-    data: 'James',
+  ip : toString(getIP()),
+  date : toString(getFullDate()),
+  time : toString(getFullTime()),
+  deviceType : toString(getDeviceType()),
+  browser : toString(getBrowser()),
+  operatingSystem : toString(getOS()),
   };
   
 function send() {
@@ -14,7 +25,12 @@ function send() {
 }
 
 function createData() {
-  let ip = GeolocationPosition(Kermit);
+  let ip = toString(getIP());
+  let date = toString(getFullDate());
+  let time = toString(getFullTime());
+  let deviceType = toString(getDeviceType());
+  let browser = toString(getBrowser());
+  let operatingSystem = toString(getOS());
 }
 
 function getIP() {
@@ -22,5 +38,95 @@ function getIP() {
             function (data) {
  
                 return data.ip;
-            })
+            });
 }
+
+function getFullDate() {
+  let fullDate = "Date:" + d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear();
+  return fullDate;
+}
+
+function getFullTime() {
+  let fullTime = "Time: " + d.getHours() + ":" + d.getMinutes() + ":" + d.getMilliseconds();
+  return fullTime;
+}
+
+const getDeviceType = () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  }
+  if (
+    /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+};
+
+function getBrowser() {
+  // Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+
+// Chrome 1 - 79
+var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+// Edge (based on chromium) detection
+var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+
+var output = 'Detecting browsers by ducktyping:<hr>';
+output += 'isFirefox: ' + isFirefox + '<br>';
+output += 'isChrome: ' + isChrome + '<br>';
+output += 'isSafari: ' + isSafari + '<br>';
+output += 'isOpera: ' + isOpera + '<br>';
+output += 'isIE: ' + isIE + '<br>';
+output += 'isEdge: ' + isEdge + '<br>';
+output += 'isEdgeChromium: ' + isEdgeChromium + '<br>';
+output += 'isBlink: ' + isBlink + '<br>';
+return output;
+}
+
+
+function getOS() {
+	let userAgent = window.navigator.userAgent,
+		platform = window.navigator.platform,
+		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+		iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+		os = null;
+
+	if (macosPlatforms.indexOf(platform) !== -1) {
+		os = 'Mac OS';
+	} else if (iosPlatforms.indexOf(platform) !== -1) {
+		os = 'iOS';
+	} else if (windowsPlatforms.indexOf(platform) !== -1) {
+		os = 'Windows';
+	} else if (/Android/.test(userAgent)) {
+		os = 'Android';
+	} else if (!os && /Linux/.test(platform)) {
+		os = 'Linux';
+	}
+
+	return os;
+}
+
+
+
